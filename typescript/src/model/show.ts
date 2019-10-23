d12dc26eb72cf8d5be09e3d13ef62ae880c573a1
Igor Soroka <stupbird@gmail.com>
2019-10-23 13:53:42 +0300

    make functional style typescript api completed

--- typescript/src/model/show.ts
+++ typescript/src/model/show.ts
@@ -1,14 +1,4 @@
-import * as mongoose from "mongoose";
-
-const uri: string = "mongodb://127.0.0.1:27017/local";
-
-mongoose.connect(uri, (err: any) => {
-  if (err) {
-    console.log(err.message);
-  } else {
-    console.log("Successfully Connected!");
-  }
-});
+import mongoose from "mongoose";
 
 export interface IShow extends mongoose.Document {
   title: string;
--- typescript/src/server.ts
+++ typescript/src/server.ts
@@ -1,6 +1,25 @@
 import app from "./app";
+import mongoose from "mongoose";
+import { MongoError } from "mongodb";
 const PORT: number = 8080;
 
-app.listen(PORT, () => {
+const uri: string | undefined = process.env.MONGO_URL;
+
+const connectToDb = async () => {
+  const connectionOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  };
+  await mongoose.connect(uri!, connectionOptions, (err: MongoError) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("MongoDB connected");
+  });
+};
+
+app.listen(PORT, async () => {
+  await connectToDb();
   console.log(`View modules app listening on port ${PORT}`);
 });
