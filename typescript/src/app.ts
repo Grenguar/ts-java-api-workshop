657b2aa7ffc06a333f3f3032be5a5492b254caae
Igor <stupbird@gmail.com>
2019-10-25 10:53:04 +0300

    Update app.ts

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -5,7 +5,6 @@ export const app: express.Application = express();
 const seriesApi = "/api/series";
 
 app.get(`${seriesApi}/`, (req: Request, res: Response) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
   res.status(200).send("Hello ts/java workshop participant!");
 });
 
