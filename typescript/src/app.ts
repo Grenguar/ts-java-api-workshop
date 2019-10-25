8a0a41609c1a1a16e0cd5a98bf974f5d64201bad
Igor <stupbird@gmail.com>
2019-10-25 13:05:45 +0300

    Update app.ts

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -2,9 +2,7 @@ import express, { Request, Response } from "express";
 
 export const app: express.Application = express();
 
-const seriesApi = "/api/series";
-
-app.get(`${seriesApi}/`, (req: Request, res: Response) => {
+app.get(`/`, (req: Request, res: Response) => {
   res.status(200).send("Hello ts/java workshop participant!");
 });
 
