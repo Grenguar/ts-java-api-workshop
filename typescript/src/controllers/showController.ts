253062d41ccd196e0eed0c4b2c02a0afeb1c331a
Igor Soroka <stupbird@gmail.com>
2019-10-28 11:14:05 +0200

    cleaned docker compose and proper mock returns to controller

--- typescript/src/controllers/showController.ts
+++ typescript/src/controllers/showController.ts
@@ -1,21 +1,21 @@
 import { Request, Response } from "express";
 
 export let allShows = (req: Request, res: Response) => {
-  res.send("Returns all TV series");
+  res.send("Returns all TV shows");
 };
 
 export let getShow = (req: Request, res: Response) => {
-  res.send("Returns one series");
+  res.send("Returns one show");
 };
 
 export let deleteShow = (req: Request, res: Response) => {
-  res.send("delete series");
+  res.send("delete show");
 };
 
 export let updateShow = (req: Request, res: Response) => {
-  res.send("update series");
+  res.send("update show");
 };
 
 export let addShow = (req: Request, res: Response) => {
-  res.send("Add series");
+  res.send("Add show");
 };
